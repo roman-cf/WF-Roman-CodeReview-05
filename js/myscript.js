8f3f6e9067b48edf5fa43a10b8faf294d19402ca
roman.cf <roman.springer@outlook.com>
2019-10-26 15:24:53 +0200

    search function ready ... code need to be cleaned up

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,28 @@
 var imgPath = "img/";
 var linkData = [["dates","contact","about","settings"],["#","#","#","#"]];
 
-var members = JSON.stringify(membersJson);
+// var members = JSON.parse(localStorage.getItem("members"));
+// if (members == "" || members == null){
+	members = JSON.stringify(membersJson);
 	members = JSON.parse(members);
+	for (let person of members) {
+		person.loveStatus = 0;
+		person.loveCounter = 0;
+	}
+// };
+members.splice(0,1);
 
-for (let person of members) {
-person.loveStatus = 0;
-person.loveCounter = 0;
-}
-
-siteBuilder();
+$(window).on('load', function() {
+    console.log("Alle geladen")
+    siteBuilder()
+})
 
 //-------------------------------------------Site Builder---------------------------------------------------
 function siteBuilder(){
 	$("body").append(`
-					<header id="header" class="d-flex justify-content-between text-white bg-info p-2 border-bottom-primary ">
+					<header id="header" class="d-flex justify-content-between text-white  bg-info p-2 border-bottom-primary ">
 						<p class="h2 m-1">Dating</p>
-						<nav class=" p-2">
+						<nav class=" navbar-collapse p-2">
 							<ul class="nav justify-content-end">
 							</ul>
 						</nav>	
@@ -35,20 +41,33 @@ function siteBuilder(){
 			<div class="row mx-auto d-flex justify-content-between my-4 p-2">
 				<h3 class=" col-lg-6 col-md-2 dropdown col-6">Candidates</h3>	  
 				<div class="dropdown col-lg-2 dropdown col-6 col-md-6">
-				    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
-				      sort candidates by ...
-				    </button>
-				    <div class="dropdown-menu">
-				      <a class="dropdown-item h4" href="#">Link 1</a>
-				      <a class="dropdown-item h4" href="#">Link 2</a>
-				      <a class="dropdown-item h4" href="#">Link 3</a>
-				    </div>
+					<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+				    	sort candidates by ...
+					</button>
+					<div class="dropdown-menu">
+				    	<p id="drpdwn1" class="dropdown-item h4" href="#">age</p>
+				    	<p id="drpdwn2" class="dropdown-item h4" href="#">loves</p>
+				    	<p class="dropdown-item h4" href="#">....</p>
+					</div>
 				</div>  		
 			</div>
 			<div id="overView" class="row"></div>
 		`)
+	//***********test**************
+	$("#drpdwn1").on("click",function(e){
+		test1("age");
+		// var testObj = numberSort();
+		// console.log(testObj);
+	})
+	$("#drpdwn2").on("click",function(e){
+		test1("loveCounter");
+		// var testObj = numberSort();
+		// console.log(testObj);
+	})
 
-	for(let i=1;i<members.length;i++){
+	//******************************
+
+	for(let i=0;i<members.length;i++){
 		overviewCardBuilder(i);
 	};	
 
@@ -63,29 +82,34 @@ function siteBuilder(){
 	for(let person of members){
 		if(person.loveStatus == 1){
 			favCardBuilder(person);
-		};
-	};	
+			};	};
+
 };
 //---------------------------------------End Site Builder---------------------------------------------------
 
-//---------------------------------------Overview Builder---------------------------------------------------
+//---------------------------------------Overview Card Builder----------------------------------------------
 function overviewCardBuilder(i){
+	let Farbe = "";
+	if(members[i].loveStatus == 1){Farbe = "-danger"}else{Farbe = "-info"};
 		$("#overView").append(`
 				<div id="card${members[i].memberId}" class="col-lg-3 col-md-6 col-sm-12 mb-3">
 					<div class="card h-100 bg-secondary"   >
 						<img class="card-img-top" src="${imgPath+members[i].picUrl}" alt="Card image"></a>
 						<div class="card-img-overlay offset-md-8 offset-lg-8">
-					 		<a href="#" id="${'member'+members[i].memberId}" class=" dateBtn btn btn-secondary ">&hearts;</a>
+					 		<p id="${'member'+members[i].memberId}" class=" dateBtn btn btn${Farbe} ">&hearts;</p>
 					 	</div>
 					 	<div class="card-body text-white">
 					    	<h4 class="card-title">${members[i].nickname}</h4>
 					    	<p class="card-text">${members[i].slogan}</p>
-					 </div>
+					 	</div>
+						<div class="progress bg-secondary">
+    						<div class="progress-bar bg${Farbe}" style="width:${members[i].loveCounter}px">${members[i].loveCounter}</div>
+						</div>
 					</div>
 				</div>
-			`);
+			`);	
 }
-//---------------------------------------End Overview Builder-----------------------------------------------
+//---------------------------------------End Overview Card Builder-------------------------------------------
 
 //---------------------------------------Favorits Card Builder----------------------------------------------
 function favCardBuilder(person){
@@ -107,59 +131,92 @@ function favCardBuilder(person){
 			</div>
 		</div>
 	`);
+	//add eventhandler
+	$("#favs"+person.memberId).on("click", function(e){
+		var tempId = Number((e.target.id).slice(4));
+		hearting(tempId);
+	});
 }
 //---------------------------------------End cards Favorits Builder-------------------------------------------
 
 //--------------------------------EventHandler HeartButton ------------------------------------------------
 $(".dateBtn").on("click", function(e){
-	
 	var tempId = Number((e.target.id).slice(6));
-	hearting(tempId,"member");
-
-	//EventHandler HeartButton in Favorits
-	$(".favsBtn").on("click", function(e){
-		console.log("vorbereitet")
-		var tempId2 = Number((e.target.id).slice(4));
-		hearting(tempId2,"favCard");
-	});
-	//End of EventHandler
-
+	hearting(tempId);
 });
 //---------------------------------------End of EventHandler-----------------------------------------------
 
 //-------------------------------------EventHandler of Like Button-----------------------------------------
-function hearting(tempId,tagId){
-	console.log(tempId)
+function hearting(tempId){
 
 	for(let person of members){	
+
 		if (person.memberId == tempId){
 
 			if(person.loveStatus==0){
+
 				person.loveStatus=1;
-				person.loveCounter++;		
-				console.log("#"+tagId+tempId);
-				$("#"+tagId+tempId).removeClass("btn-secondary");
-				$("#"+tagId+tempId).addClass("btn-danger");
+				person.loveCounter++;
+				let progress = person.loveCounter;
+				$("#card"+tempId+" .progress-bar").css("width", progress).text(progress).removeClass("bg-info").addClass("bg-danger");		
+				$("#member"+tempId).removeClass("btn-info");
+				$("#member"+tempId).addClass("btn-danger");
 
 				favCardBuilder(person);
+				store();
 
 			}else{
 
 				person.loveStatus=0;
-
+				$("#card"+tempId+" .progress-bar").removeClass("bg-danger").addClass("bg-info");		
 				$("#member"+tempId).removeClass("btn-danger");
-				$("#member"+tempId).addClass("btn-secondary");
-
+				$("#member"+tempId).addClass("btn-info");
+				$("#favCard"+tempId).unbind()
 				$("#favCard"+tempId).remove();
+				store();
 			}
 		}
 	}
 };
 //-------------------------End-of------EventHandler of Like Button-----------------------------------------
 
-function favHearting(e){
-	let tempId=	e.slice(4);
-	person.loveStatus=0;
-	console.log(tempId+" "+person.loveStatus+" "+e);
-	$("#favCard"+tempId);
+//--------------------------store-local-------------------------------------------------------------------
+function store(){ 
+	localStorage.setItem("members", JSON.stringify(members));
 }
+//-------------------ende-store-local----------------------------------------------------------------------
+
+//--------------------------Number Sort Function------------------------------------------------------------
+function numberSort(wert){
+	members.sort(function(a, b){
+	return b[wert]-a[wert];
+	});
+}
+//------------------End----Number Sort Function------------------------------------------------------------
+
+//------------------Find Position in Obj which have a special Value for specific key-----------------------
+function positionFinder(obj, attr, value) {
+    for(var i = 0; i < obj.length; i ++) {
+        if(obj[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+//--------------------------End Find Position-----------------------------------------------------
+
+//------------------Sort Candidates------------------------------------------------------------------------
+function test1(searchPara){
+	$("#overView").empty();
+
+	numberSort(searchPara);
+
+	for(let i=0;i<members.length;i++){
+		overviewCardBuilder(i);
+	};
+	$(".dateBtn").on("click", function(e){
+		var tempId = Number((e.target.id).slice(6));
+		hearting(tempId);
+	});
+};
+//------------------Sort Candidates------------------------------------------------------------------------
